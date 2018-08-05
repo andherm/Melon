6f26afb9d01cb85d0a84502213739f0cfe3d52c0
Andreas Hermansson <andreashermansson@Andreass-MacBook-Pro.local>
2018-08-05 21:31:06 +0200

    Lite nya grejer på sidan, typ grafik och databas och lite sånt

--- databas.py
+++ databas.py
@@ -1,28 +1,7 @@
-# Andreas dunderkod, johan är noob
 from __future__ import print_function
 from apiclient.discovery import build
 from httplib2 import Http
 from oauth2client import file as oauth_file, client, tools
-
-from flask import render_template
-from flask import Flask
-app = Flask("Server")
-
-@app.route("/")
-def hello():
-	return render_template('hemsida.html')
-
-@app.route("/traininghours")
-def traininghours():
-	return render_template('traininghours.html', database=fetchdata())
-
-@app.route("/testresults")
-def testresults():
-	return render_template('testresults.html')
-
-
-
-
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly'
@@ -33,7 +12,8 @@ SAMPLE_RANGE_JOHAN = 'Johan!A2:B'
 SAMPLE_RANGE_ANDREAS = 'Andreas!A2:B'
 
 
-def fetchdata():
+
+def main():
     """Shows basic usage of the Sheets API.
 
     Prints values from a sample spreadsheet.
@@ -46,32 +26,33 @@ def fetchdata():
     service = build('sheets', 'v4', http=creds.authorize(Http()))
 
     # Call the Sheets API
-    sum_minutes_johan = 0
+    print(SAMPLE_RANGE_JOHAN.split("!")[0])
     result = service.spreadsheets().values().get(
         spreadsheetId=SAMPLE_SPREADSHEET_ID,
         range=SAMPLE_RANGE_JOHAN).execute()
-    values_johan = result.get('values', [])
-    for rows in values_johan:
-    	sum_minutes_johan = int(rows[1])+sum_minutes_johan
-    print(sum_minutes_johan)
-
+    values = result.get('values', [])
+    if not values:
+        print('No data found.')
+    else:
+        print('Datum, Minuter:')
+        for row in values:
+            # Print columns A and E, which correspond to indices 0 and 4.
+            print(u'%s, %s' % (row[0], row[1]))
 
    # Call the Sheets API
-    sum_minutes_andreas = 0
+    print(SAMPLE_RANGE_ANDREAS.split("!")[0])
     result = service.spreadsheets().values().get(
         spreadsheetId=SAMPLE_SPREADSHEET_ID,
         range=SAMPLE_RANGE_ANDREAS).execute()
-    values_andreas = result.get('values', [])
-    for rows in values_andreas:
-    	sum_minutes_andreas = int(rows[1])+sum_minutes_andreas
-    print(sum_minutes_andreas)
-
-    return [sum_minutes_johan, sum_minutes_andreas]
-
-
+    values = result.get('values', [])
+    if not values:
+        print('No data found.')
+    else:
+        print('Datum, Minuter:')
+        for row in values:
+            # Print columns A and E, which correspond to indices 0 and 4.
+            print(u'%s, %s' % (row[0], row[1]))
 
 
 if __name__ == '__main__':
-	app.run(debug=True)
-
-
+    main()
